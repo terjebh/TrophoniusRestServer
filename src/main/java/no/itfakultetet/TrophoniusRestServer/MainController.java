f85fc7bf93cb8e8693404b3849954762548b621c
Terje Berg-hansen <terje@itfakultetet.no>
2020-03-22 02:28:26 +0100

    Initial commit

---
+++ src/main/java/no/itfakultetet/TrophoniusRestServer/MainController.java
@@ -0,0 +1,22 @@
+package no.itfakultetet.TrophoniusRestServer;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("<h1>Hello %s!</h1>", name);
+    }
+
+    @GetMapping("/user")
+    public String user(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("<h1>User Data</h1>", name);
+    }
+
+
+}
---
+++ src/main/java/no/itfakultetet/TrophoniusRestServer/TrophoniusRestServerApplication.java
@@ -0,0 +1,13 @@
+package no.itfakultetet.TrophoniusRestServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrophoniusRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrophoniusRestServerApplication.class, args);
+	}
+
+}
